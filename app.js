18b1d2c1e9ebd57e9944a1b9a4fa0521e728b5ec
Carlos Torres <Carlos@torres.com.co>
2026-05-28 09:45:16 -0500

    eliminando dotenv para render

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const AWS = require('aws-sdk');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
 
