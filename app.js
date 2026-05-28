767bd051cce6ad1fd7ba1261446e0c364ceedc36
Carlos Torres <Carlos@torres.com.co>
2026-05-28 10:08:49 -0500

    force render redeploy remove dotenv

--- app.js
+++ app.js
@@ -130,4 +130,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("APP INICIANDO...");
     console.log("Servidor funcionando en puerto", PORT);
-});
+});// force redeploy fix
